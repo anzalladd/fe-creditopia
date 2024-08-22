92f0e4fbd18743f0394ca481438e143b8d35c59a
Anzalla Dzikri Dhamara <anzalladd@gmail.com>
2024-08-22 11:35:11 +0800

    init project

--- src/frontend-creditopia-frontend/src/App.vue
+++ src/frontend-creditopia-frontend/src/App.vue
@@ -1,28 +1,10 @@
-<script setup>
-import { ref } from 'vue';
-import { frontend_creditopia_backend } from 'declarations/frontend-creditopia-backend/index';
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await frontend_creditopia_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
-}
-</script>
-
 <template>
-  <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
-  </main>
+  <div>
+    <Navbar />
+    <router-view></router-view>
+  </div>
 </template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
--- src/frontend-creditopia-frontend/src/main.js
+++ src/frontend-creditopia-frontend/src/main.js
@@ -1,6 +1,28 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
-import './index.scss';
 import App from './App.vue';
+import { createRouter, createMemoryHistory } from 'vue-router'
+import HomeView from './pages/HomeView.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css'
+import './index.css';
 
-createApp(App).use(createPinia()).mount('#app');
+const routes = [
+  { path: '/', component: HomeView },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+createApp(App)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura
+    }
+  })
+  .use(router)
+  .use(createPinia())
+  .mount('#app');
